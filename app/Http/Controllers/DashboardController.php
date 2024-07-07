184b127589ef58a190f6648642d8739cd5ba2402
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-07-07 12:49:47 +0700

    Group By

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,6 @@ class DashboardController extends Controller
             ->whereRaw('MONTH(tgl_presensi)="' . $month . '"')
             ->whereRaw('YEAR(tgl_presensi)="' . $year . '"')
             ->orderBy('tgl_presensi', 'asc')
-            ->groupBy('u_id')
             ->get();
 
         $hadir      = DB::table('presensi')
@@ -29,7 +28,6 @@ class DashboardController extends Controller
             ->whereRaw('MONTH(tgl_presensi)="' . $month . '"')
             ->whereRaw('YEAR(tgl_presensi)="' . $year . '"')
             ->orderBy('tgl_presensi', 'asc')
-            ->groupBy('u_id')
             ->first();
 
         $cuti      = DB::table('pengajuan_cuti')
@@ -39,7 +37,6 @@ class DashboardController extends Controller
             ->whereRaw('MONTH(tgl_izin)="' . $month . '"')
             ->whereRaw('YEAR(tgl_izin)="' . $year . '"')
             ->orderBy('tgl_izin', 'asc')
-            ->groupBy('u_id')
             ->first();
 
         $sakit      = DB::table('pengajuan_cuti')
@@ -49,7 +46,6 @@ class DashboardController extends Controller
             ->whereRaw('MONTH(tgl_izin)="' . $month . '"')
             ->whereRaw('YEAR(tgl_izin)="' . $year . '"')
             ->orderBy('tgl_izin', 'asc')
-            ->groupBy('u_id')
             ->first();
 
         $terlambat   = DB::table('presensi')
@@ -58,7 +54,6 @@ class DashboardController extends Controller
             ->whereRaw('MONTH(tgl_presensi)="' . $month . '"')
             ->whereRaw('YEAR(tgl_presensi)="' . $year . '"')
             ->orderBy('tgl_presensi', 'asc')
-            ->groupBy('u_id')
             ->get();
         $data = [
             'title'             => 'dashboard',

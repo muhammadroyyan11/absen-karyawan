5883b64ed9fd8235166fa23755f924093ddc057d
Dikifebrian24 <dikifebrian04@gmail.com>
2025-05-18 01:30:04 +0700

    tes

--- resources/views/layout/admin/admin.blade.php
+++ resources/views/layout/admin/admin.blade.php
@@ -52,7 +52,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.0
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        <strong>Copyright &copy; 2025 <a href="https://jezpro.id">PT. ZONA KARYA NUSANTARA</a>.</strong> All rights
         reserved.
     </footer>
 
